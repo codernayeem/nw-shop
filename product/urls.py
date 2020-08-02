818efe41b1a97093618a3260e9c624e881b45b04
Md Nayeem <codernayeem898@gmail.com>
2020-08-02 22:10:13 +0600

    Added url for categories

--- product/urls.py
+++ product/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from .views import categories_view
 
 urlpatterns = [
-
+    path('categories/', categories_view, name='categories'),
 ]
