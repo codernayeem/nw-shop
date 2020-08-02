2188e99cf4064d9cbc98ea1b40bede132c0f2d31
Md Nayeem <codernayeem898@gmail.com>
2020-08-02 17:08:42 +0600

    Added Product Model

--- product/models.py
+++ product/models.py
@@ -9,3 +9,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True, default=1001, unique=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=2000, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)
+    unit_system = models.CharField(max_length=255, choices=(('kg', 'KG'), ('item', 'Item'), ('both', 'KG & Item'), ))
+    price_per_kg = models.FloatField(default=0)
+    price_per_item = models.FloatField(default=0)
+    minimum_buy_in_kg = models.FloatField(default=1)
+    maximum_buy_in_kg = models.FloatField(default=10)
+    minimum_buy_in_item = models.FloatField(default=5)
+    maximum_buy_in_item = models.FloatField(default=20)
+    icon_url = models.CharField(max_length=2083)
+
+    def __str__(self):
+        return self.name
