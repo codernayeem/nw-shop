10e905d5e6c67328d5a901ce817905ca454f7a05
Md Nayeem <codernayeem898@gmail.com>
2020-08-02 22:09:49 +0600

    Added categories_view in views.py

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Category
 
-# Create your views here.
+
+def categories_view(request):
+    categories = Category.objects.all()
+
+    return render(request, 'categories.html', {'categories': categories})
