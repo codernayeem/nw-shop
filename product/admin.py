12bb51682e89c1411ef7f490e795e3180eea6cc4
Md Nayeem <codernayeem898@gmail.com>
2020-08-02 17:09:53 +0600

    Added Product Model to Admin

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -7,4 +7,24 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'active')
 
 
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'name']
+    list_display = ('id', 'name', 'category', 'unit_system', 'price_per_kg', 'price_per_item', 'stock')
+    fieldsets = (
+        (None, {
+            'fields': ('id', 'name', 'description', 'category', 'stock', 'unit_system')
+        }),
+        ('Unit KG', {
+            'fields': ('price_per_kg', 'minimum_buy_in_kg', 'maximum_buy_in_kg'),
+        }),
+        ('Unit Item', {
+            'fields': ('price_per_item', 'minimum_buy_in_item', 'maximum_buy_in_item'),
+        }),
+        (None, {
+            'fields': ('icon_url',)
+        }),
+    )
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
