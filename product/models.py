9e7b82d1f0246dfd5d9bd25dbf272a42cb03c0ad
Md Nayeem <codernayeem898@gmail.com>
2020-08-02 16:54:40 +0600

    Added Category Model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(max_length=2000, blank=True, null=True)
+    icon_url = models.CharField(max_length=2083, blank=True, null=True)
+    active = models.BooleanField(default=False, help_text='If not active, this category will be hidden in category page.')
+
+    def __str__(self):
+        return self.name
